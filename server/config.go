67d1f5b70fd51836a8bad517f5a46bbef0835657
Galaxyobe <galaxyobe@qq.com>
2020-06-11 14:34:02 +0800

    add domain into config

--- server/config.go
+++ server/config.go
@@ -3,6 +3,8 @@ package server
 type Config struct {
 	// TCP address to listen on, ":http" if empty
 	Addr string `json:"addr" yaml:"addr"`
+	// Domain name
+	Domain string `json:"domain" yaml:"domain"`
 
 	CertFile string `json:"cert_file" yaml:"cert_file"`
 	KeyFile  string `json:"key_file" yaml:"key_file"`
